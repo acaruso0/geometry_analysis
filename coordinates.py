44aba241534e307191d77309279f6ba67766f38c
Alessandro Caruso <acaruso@ucsd.edu>
2019-08-07 14:32:41 -0700

    added slicing functionality

--- coordinates.py
+++ coordinates.py
@@ -2,9 +2,10 @@ import numpy as np
 
 class XYZfile():
     def __init__(self, filename):
-        self._frames, self.energies, self.nat = self.loadall(filename)
+        self.frames, self.energies, self.nat = self.loadall(filename)
         self.nframes = self.nframes()
         self.atomtypes = self.atomtypes()
+        self.atcount = self.atcount()
 
     def loadall(self, filename):
         with open(filename, 'r') as xyzfile:
@@ -20,21 +21,53 @@ class XYZfile():
         return frames, energies, nat
 
     def nframes(self):
-        return len(self._frames)
+        return len(self.frames)
+
+    def atcount(self):
+        atcount = {}
+        for atomtype in self.atomtypes:
+            it = 0
+            for at in self.frames[0]:
+                if atomtype == at.split('_')[0]:
+                    it += 1
+            atcount[atomtype] = it
+        return atcount
 
     def atomtypes(self):
-        if not self._frames:
+        if not self.frames:
             print("Make sure to load a trajectory first!")
         else:
             atomtypes = set()
-            for at in self._frames[0]:
+            for at in self.frames[0]:
                 atomtypes.add(at.split('_')[0])
             return atomtypes
 
-    def coordinates(self, frame):
+    def coordprint(self, frame, label = False):
         print(F" {self.nat}\n {' '.join(self.energies[frame].split())}")
-        for at in self._frames[frame]:
-            print(F"{at.split('_')[0].ljust(3)} {self._frames[frame][at][0]} {self._frames[frame][at][1]} {self._frames[frame][at][2]}")#   {:.3f}")
+        for at in self.frames[frame]:
+            if label == False:
+                print(F"{at.split('_')[0].ljust(3)} {self.frames[frame][at][0]} {self.frames[frame][at][1]} {self.frames[frame][at][2]}")#   {:.3f}")
+            else:
+                print(F"{at.split('_')[0]}.{at.split('_')[1]} {self.frames[frame][at][0]} {self.frames[frame][at][1]} {self.frames[frame][at][2]}")
+
+    def getatom(self, in_frame, end_frame, *args):
+        arglist, sliced = [], []
+        if args:
+            for arg in args:
+                arg = arg.split('.')
+                if ':' in arg[1]:
+                    argrange = range(int(arg[1].split(':')[0]), int(arg[1].split(':')[1]) + 1)
+                arglist += [arg[0] + "_" + str(n) for n in list(argrange)]
+        else:
+            arglist = list(self.frames[0].keys())#[in_frame:end_frame+1][0].keys())
+
+        for frame in range(in_frame, end_frame + 1):
+            sl_atoms = {}
+            for atlabel in arglist:
+                sl_atoms[atlabel] = self.frames[frame][atlabel]
+            sliced.append(sl_atoms)
+            
+        return sliced
 
 #    def __del__(self):
 #        self.handle.close()
--- geometry_analysis.py
+++ geometry_analysis.py
@@ -4,9 +4,14 @@ import coordinates as crd
 if __name__ == "__main__":
     testXYZ = crd.XYZfile("input2.xyz")
 
-#    print(testXYZ._frames)
+#    print(testXYZ.frames)
 #    print(testXYZ.atomtypes)
 #    print(testXYZ.nframes)
-    for n in range(testXYZ.nframes):
-        testXYZ.energies[n] = F'{testXYZ.energies[n]} {testXYZ.energies[n]} 0.0 0.0'
-        testXYZ.coordinates(n)
+#    for n in range(testXYZ.nframes):
+#        testXYZ.energies[n] = F'{testXYZ.energies[n]} {testXYZ.energies[n]} 0.0 0.0'
+#        testXYZ.coordprint(n)
+#        testXYZ.coordprint(n, label = True)
+#    print(testXYZ.atcount)
+#    print(testXYZ.getatom([1], 'O.2'))
+    print(testXYZ.getatom(2, 2, 'H.1:2'))
+    print(testXYZ.getatom(1, 2))
